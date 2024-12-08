bf1ca7380b5026e08fdf12a6192c1791e083b924
Robin Barooah <robin@sublime.org>
2024-12-07 18:30:21 -0800

    use png as an intermediate step

--- showpdf.py
+++ showpdf.py
@@ -13,11 +13,16 @@ pdf_path = sys.argv[1]
 with tempfile.NamedTemporaryFile(suffix='.bmp', delete=False) as temp_bmp:
     bmp_filename = temp_bmp.name
 
-# Convert PDF to BMP using pdftocairo
-subprocess.run(['pdftocairo', '-bmp', '-singlefile', '-f', '1', '-l', '1', '-r', '300', pdf_path, bmp_filename[:-4]])
+# Convert PDF to PNG using pdftocairo
+png_filename = bmp_filename.replace('.bmp', '.png')
+subprocess.run(['pdftocairo', '-png', '-singlefile', '-f', '1', '-l', '1', '-r', '300', pdf_path, png_filename])
+
+# Convert PNG to BMP using ImageMagick's convert command
+subprocess.run(['convert', png_filename, bmp_filename])
 
 # Execute render_bmp
 subprocess.run(['./render_bmp', '0', '0', bmp_filename])
 
-# Clean up temporary file
+# Clean up temporary files
 os.remove(bmp_filename)
+os.remove(png_filename)
